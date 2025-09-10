783a9b46098fa1d23c25cb44310adc5a63aaef15
Vrushank2808 <vrushankgotawala@gmail.com>
2025-09-10 17:55:18 +0530

    Refactor API handler export and update Vercel configuration for improved structure and clarity

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('../server');
 
-exports.handler = serverlessExpress({ app });
+module.exports = serverlessExpress({ app });
